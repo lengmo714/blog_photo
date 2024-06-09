ef01661057077c68454fb2f1f62f7c172657e3b4
曹雪锋 <842673413@qq.com>
2024-06-09 18:05:29 +0800

    add photos

--- tool.py
+++ tool.py
@@ -168,7 +168,7 @@ def git_operation():
     '''
     os.system('git add --all')
     os.system('git commit -m "add photos"')
-    os.system('git push origin master')
+    os.system('git push')
 
 
 def bubble(bubbleList):
